73cf3d72d604515d8e13ba40d43bff8b0b293d93
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-02-13 10:05:46 +0100

    New branch with sign up fragment and a "User class"

---
+++ app/src/main/java/com/example/foodhero/struct/User.kt
@@ -0,0 +1,17 @@
+package com.example.foodhero.struct
+
+class User (
+    val email:String?=null,
+    val phoneNumber:String?=null,
+    val adress:String?=null,
+    val socialSecurityNumber:String?=null,
+    val password:String?=null, ):java.io.Serializable
+         {
+             override fun toString(): String {
+                 return "\n" +
+                         "Email: $email\n" +
+                         "Phonenumber: $phoneNumber\n" +
+                         "Adress: $adress\n" +
+                         "Socailsecuritynumber: $socialSecurityNumber\n" +
+                         "Password: $password\n"}
+}
