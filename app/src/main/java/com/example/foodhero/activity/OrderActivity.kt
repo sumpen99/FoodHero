cb913ec6efc5936fe9e77673dc35bbd7290941bd
a <yhma22.alkeal@folkuniversitetet.nu>
2023-02-15 14:53:36 +0100

    Constraints fix på konto sidan.

---
+++ app/src/main/java/com/example/foodhero/activity/OrderActivity.kt
@@ -0,0 +1,24 @@
+package com.example.foodhero.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodhero.R
+
+class OrderActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+
+
+
+
+
+
+
+
+    }
+}
