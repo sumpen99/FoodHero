3e7a0e436a8fddff9533cfc009160b5b4905646f
roberto.potrus <roberto.potrus@followit.se>
2023-02-20 13:34:17 +0100

    save to firestore

--- app/src/main/java/com/example/foodhero/activity/AdminActivity.kt
+++ app/src/main/java/com/example/foodhero/activity/AdminActivity.kt
@@ -2,15 +2,18 @@ package com.example.foodhero.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import com.example.foodhero.R
 import com.example.foodhero.database.AuthRepo
+import com.example.foodhero.database.FirestoreRepository
+import com.example.foodhero.struct.Restaurant
 
 class AdminActivity : AppCompatActivity() {
     private val auth = AuthRepo()
+    private val firestore = FirestoreRepository();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +23,10 @@ class AdminActivity : AppCompatActivity() {
 
         val backButton = findViewById<ImageButton>(R.id.BackButtonImage)
         val logoutButton = findViewById<ImageButton>(R.id.LogoutImageButton)
+        val submitButton = findViewById<Button>(R.id.SubmitButton)
         backButton.setOnClickListener(clickListener)
         logoutButton.setOnClickListener(clickListener)
+        submitButton.setOnClickListener(clickListener)
     }
 
     private val clickListener: View.OnClickListener = View.OnClickListener { view ->
@@ -31,6 +36,13 @@ class AdminActivity : AppCompatActivity() {
             }
             R.id.LogoutImageButton -> {
                 auth.signOut()
+            }
+            R.id.SubmitButton -> {
+                val nameField = findViewById<EditText>(R.id.ResturantNameEditText)
+                val locationField = findViewById<EditText>(R.id.LocationEditText)
+                val descriptionField = findViewById<EditText>(R.id.DescriptionEditText)
+                val restaurant = Restaurant(name = nameField.text.toString(), adress = locationField.text.toString(), description = descriptionField.text.toString())
+                firestore.saveRestaurant(restaurant)
             }
         }
     }
