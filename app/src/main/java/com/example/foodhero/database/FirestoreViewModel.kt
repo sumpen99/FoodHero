67f0644b868a7efd670d8a22f82d4cc67e39ffae
sumpen99 <sumpen841001@hotmail.com>
2023-02-22 19:23:30 +0100

    behåll aktiviteter levanda

--- app/src/main/java/com/example/foodhero/database/FirestoreViewModel.kt
+++ app/src/main/java/com/example/foodhero/database/FirestoreViewModel.kt
@@ -12,7 +12,9 @@ import com.firebase.geofire.GeoFireUtils
 import com.firebase.geofire.GeoLocation
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
-import com.google.firebase.firestore.*
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.coroutines.tasks.await
 
 class FirestoreViewModel {
