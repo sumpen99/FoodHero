eb9c43f8c4d5351dbda3816fcd1f89d2f3cffcac
a <yhma22.alkeal@folkuniversitetet.nu>
2023-02-23 21:41:06 +0100

    restAdmin sidorna fixade

---
+++ app/src/main/java/com/example/foodhero/activity/RestAdminActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodhero.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodhero.R
+
+class RestAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rest_admin)
+    }
+}
